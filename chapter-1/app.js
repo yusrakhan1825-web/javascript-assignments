2eca72b7b8e3e446d30102bc978a748f7d386c58
yusrakhan1825-web <yusrakhan1825@gmail.com>
2026-07-27 17:32:03 +0500

    javascript

---
+++ chapter-1/app.js
@@ -0,0 +1,8 @@
+    // question 2 make alert
+    alert("Welcome to JS..");
+
+    // question 3 make alert
+    alert("Happy Coding !");
+
+    // question 4 make alert
+    alert("Hello...I can run js through my web browser's console")
---
+++ chapter-2/app.js
@@ -0,0 +1,27 @@
+// Q:1 declare variable called username
+
+var username;
+
+//Q:2 declare variable called myName
+
+var myName = "yusra";
+ 
+// Q:3 JS variable titled message
+// a.
+var message;
+// b.
+var message = "Hello World";
+// c.
+alert ("Hello World");
+
+// Q:4 save data of student and make alert
+
+var studentName = "yusra khan";
+var studentAge = "15 years old";
+var studentSubject = "Certified Mobile application Development";
+
+// make alerts of above data
+
+alert ("yusra khan");
+alert ("!5 years old");
+alert ("Certified Mobile application Development")
