3a71c1d31cc1aaa046bf5b359210bbf6cbe74a6b
yusrakhan1825-web <yusrakhan1825@gmail.com>
2026-08-06 02:18:51 +0500

    completed

---
+++ chapter-12-13/app.js
@@ -0,0 +1,69 @@
+// Q-1
+
+var letter = prompt("Enter a character (number, uppercase, or lowercase letter):");
+
+
+// Q-2
+
+var num1 = prompt("Enter the first integer:");
+var num2 = prompt("Enter the second integer:");
+
+
+if (num1 > num2) {
+    alert("The larger number is: " + num1);
+} else if (num2 > num1) {
+    alert("The larger number is: " + num2);
+} else {
+    alert("Both integers are equal.");
+}
+
+// Q-3
+
+var number = prompt("Enter a number:");
+if (number > 0) {
+    alert("The number is positive.");
+} else if (number < 0) {
+    alert("The number is negative.");
+} else if (number === 0) {
+    alert("The number is zero.");
+} else {
+  alert("Invalid numeric input.");
+}
+
+// Q-4
+
+// Q-5
+
+var correctPassword = "yusrakhan11";
+var userPassword = prompt("Please enter your password:");
+if(userPassword === correctPassword) {
+   alert("Correct! The password you entered matches the original password.");
+} else {
+    alert("Incorrect password");
+}
+
+// Q-6
+
+var greeting;
+var hour = 13;
+if (hour < 18) {
+    greeting = "Good day";
+} else {
+    greeting = "Good evening";
+}
+
+// Q-7
+
+
+var time = prompt("Enter time in 24-hour clock format (e.g., 1900 for 7 PM):");
+if (time >= 0000 && time < 1200) {
+    alert("Good morning!");
+} else if (time >= 1200 && time < 1700) {
+    alert("Good afternoon!");
+} else if (time >= 1700 && time < 2100) {
+    alert("Good evening!");
+} else if (time >= 2100 && time <= 2359) {
+    alert("Good night!");
+} else {
+    alert("Invalid time!");
+}
