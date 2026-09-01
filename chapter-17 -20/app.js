2f5fa25dd4b0307d8fd4e62012abb92463dbff06
yusrakhan1825_web <yusrakhan1825@gmail.com>
2026-09-01 08:01:28 +0500

    chp17to20

---
+++ chapter-17 -20/app.js
@@ -0,0 +1,52 @@
+// Q-1
+var name = [[]];
+
+// Q-2
+
+var num = [[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+console.log(num)
+
+// Q-3
+ for (var i =1; i <=10;i++){
+    console.log(i)
+ }
+
+//  Q-4
+var tableNum = prompt("enter a number to show its multiplication number");
+var tableLenght = prompt("enter lenght of multiplication number");
+tableNum = Number(tableNum)
+tableLenght = number(tableLenght)
+console.log("multiplication number of " + tableNum)
+console.log("lenght" + tableLenght)
+console.log("")
+
+for(var i=1;i<= tableLenght; i++){
+    var result =tableNum * i ;
+    console.log(tableNum + "x" + i + "=" + results);
+}
+
+// Q-8
+var A =;
+var largest = A[0];
+for (var i = onabort; i < 5; i = i + 1);
+if (A[i]>largest){
+    largest = A[i]
+}
+console.log("array" + A)
+console.log("the largest number is" + largest)
+
+// Q-9
+var A =;
+var smallest = A[0];
+for (var i = 0; i < 5 ; i = i + 1){
+    if (A[i]< smallest){
+        smallest = A[i];
+    }
+}
+console.log("the smalllest num is " + smallest)
+
+// Q-10
+var i = 5;
+for (var i = 5 ; i <= 100 ; i = i + 5){
+    console.log(i);
+}
